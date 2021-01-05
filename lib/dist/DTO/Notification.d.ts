32836ca83995116c194cb4a846a0150ccff2923d
Iagor Moraes <iagor2014@outlook.com>
2021-01-04 22:11:03 -0300

    build fix

---
+++ lib/dist/DTO/Notification.d.ts
@@ -0,0 +1,14 @@
+export declare class Notification {
+    identifier: string;
+    payload: any;
+    constructor(payload: object);
+    get title(): string;
+    get body(): string;
+    get sound(): string;
+    get badge(): number;
+    get type(): string;
+    get thread(): string;
+    get importance(): number;
+    get channelName(): string;
+    get channelId(): string;
+}
